dd4117be72093ff208bbf6c84724858a4d70c89b
Flavio Medeiros <flaviomotamedeiros@gmail.com>
2015-12-06 19:44:10 -0300

    Removendo projeto com problema, vou adicionar o projeto novamente.

--- patterns/list2/h20/refactoring6/example1.c
+++
@@ -1,8 +0,0 @@
-if (conf.memcached.host == NULL && (conf.cache.setup == setup_cache_memcached
-#if H2O_USE_SESSION_TICKETS
-                                        || conf.ticket.update_thread == ticket_memcached_updater
-#endif
-                                        )) {
-        h2o_configurator_errprintf(cmd, node, "configuration of memcached is missing");
-        return -1;
-    }
